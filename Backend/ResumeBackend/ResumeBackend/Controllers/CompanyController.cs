06bf174f0ae0dfa5b73f2784690feecdd68da304
Chalana Kalpitha <chalana@bistecglobal.com>
2023-12-25 13:30:45 +0530

    add migration

---
+++ Backend/ResumeBackend/ResumeBackend/Controllers/CompanyController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ResumeBackend.Core.Context;
+
+namespace ResumeBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompanyController : ControllerBase
+    {
+        private ApplicationDbContext _context { get; }
+
+        public CompanyController(ApplicationDbContext context)
+        {
+            context = _context;
+        }
+
+
+    }
+}
