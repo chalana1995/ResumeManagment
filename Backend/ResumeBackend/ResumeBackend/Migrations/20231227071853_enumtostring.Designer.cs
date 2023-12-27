87b33ff3e4111acfc9e2e6382a5f23e8d9b52ca5
Chalana Kalpitha <chalana@bistecglobal.com>
2023-12-27 13:17:16 +0530

    add migration

--- Backend/ResumeBackend/ResumeBackend/Migrations/20231227071853_enumtostring.Designer.cs
+++ Backend/ResumeBackend/ResumeBackend/Migrations/20231227071853_enumtostring.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ResumeBackend.Core.Context;
 
@@ -11,9 +12,11 @@ using ResumeBackend.Core.Context;
 namespace ResumeBackend.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231227071853_enumtostring")]
+    partial class enumtostring
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
