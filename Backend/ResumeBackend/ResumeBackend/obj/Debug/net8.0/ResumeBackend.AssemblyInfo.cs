4f641f4dc94a912ef97dc786b7262ed06f401e9c
chalana kalpitha <chalanakalpitha72@gmail.com>
2024-01-01 23:26:39 +0530

    new file

--- Backend/ResumeBackend/ResumeBackend/obj/Debug/net8.0/ResumeBackend.AssemblyInfo.cs
+++ Backend/ResumeBackend/ResumeBackend/obj/Debug/net8.0/ResumeBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ResumeBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ccc3176837776511772fcc83a2546800ba93a93")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ResumeBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ResumeBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
