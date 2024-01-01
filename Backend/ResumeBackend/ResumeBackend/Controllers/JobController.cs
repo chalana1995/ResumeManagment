0425e1b3a935afe2230eae0fc3512f7510fa78b4
chalana kalpitha <chalanakalpitha72@gmail.com>
2024-01-01 23:24:29 +0530

    add new controller job

---
+++ Backend/ResumeBackend/ResumeBackend/Controllers/JobController.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ResumeBackend.Core.Context;
+using ResumeBackend.Core.Dtos.Job;
+using ResumeBackend.Core.Entities;
+
+namespace ResumeBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobController : ControllerBase
+    {
+        private ApplicationDbContext _context { get; }
+        private IMapper _mapper { get; }
+
+        public JobController(ApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        [Route("Create")]
+        public async Task<IActionResult> CreateJob([FromBody]JobCreateDto dto)
+        {
+            var newJob = _mapper.Map<Job>(dto);
+            await _context.Jobs.AddAsync(newJob);
+            await _context.SaveChangesAsync();
+
+            return Ok("Job Created Succefully");
+
+        }
+    }
+}
